7ba5c368fe66737b1b6eadefd656fe6861f4a9b7
Jar4ikm <119585243+Jar4ikm@users.noreply.github.com>
2024-04-10 21:59:35 +0300

    Update sql_laused.sql
    Конспект был приведён в нормальный вид. Убраны лишние строки, добавлены описание для команд.

--- sql_laused.sql
+++ sql_laused.sql
@@ -1,128 +1,34 @@
 MS SQL Server managment studio
 Server Name: localdb/(...) & windows auth
-Primary key - Делает каждый предмет уникальным
-FK - FOREIGN KEY
-CREATE DATABASE murdveetitpv22;
+  
+PK - Primary key
+FK - Foreign key
+  
+CREATE DATABASE base_name;
 
-CREATE DATABASE murdveetitpv22;
+USE base_name;
+-- Пример создания таблицы:
+CREATE TABLE table_name(
+ID int Primary key identity(1,1),
+var_char_name varchar(50) UNIQUE,
+int_name int NOT NULL,
+text_name Text,
+date_name date);
 
-USE murdveetitpv22;
-CREATE TABLE oppeaine(
-oppeaineID int Primary key identity(1,1),
-nimetus varchar(50) UNIQUE,
-kestvus int NOT NULL,
-kirjuldus Text,
-algus_kuupaev date);
---Kuva tabeli Отобрази таблицу
+-- Удаление таблицы
+DROP table table_name;
 
+-- Добавить запись в таблицу
+INSERT INTO table_name(var_char_name,int_name,date_name) VALUES ('short_text',15,'2024-08-20');
 
---delete table
---DROP table oppeaine;
+-- Изменить запись
+UPDATE table_name SET text_name=':))' where var_char_name='short_text';
+UPDATE table_name SET text_name=':))' where text_name IS NULL;
 
---Andmete lisamine tabeli sisse
-INSERT INTO oppeaine(nimetus, kestvus, kirjuldus, algus_kuupaev, opetajaID) VALUES ('Lifnux',2,'sudo rm -rf /*', '2125-12-9',555);
---Opitaja table
-CREATE TABLE opetaja(
-opetajaID int Primary key identity(1,1),
-eesnimi varchar(50) NOT NULL,
-perenimi varchar(50) NOT NULL,
-sünniaeg date,
-kontakt varchar(50));
+-- Добавление строки в таблицу
+ALTER TABLE table_name ADD added_name_for_FK varchar(50);
 
-INSERT INTO opetaja(eesnimi, perenimi, kontakt) VALUES ('Mikhail','Agapov','tg +7 910 4444116');
-
---update kirje
-UPDATE opetaja SET sünniaeg='1999-2-2' where opetajaID=3;
+-- Добавление вторичного ключа, FOREIGN KEY
+ALTER TABLE table_name ADD FOREIGN KEY (added_name_for_FK) REFERENCES second_table(target_for_FK);
 
 
-
---Tabeli muutmine veergu lisamine
--- FK -> PK
-ALTER TABLE oppeaine ADD opetajaID int;
-
-SELECT * FROM opetaja;
-SELECT * FROM oppeaine;
-UPDATE opetaja SET opetajaID=2 where opetajaID = 3 ;
-
--- FK add
-ALTER TABLE oppeaine ADD FOREIGN KEY (opetajaID) REFERENCES opetaja(opetajaID);
-
-
-
-
-
-
-
-
-
-
-
-
-
-------
-CREATE DATABASE murdveetitpv22;
-
-USE murdveetitpv22;
-CREATE TABLE oppeaine(
-oppeaineID int Primary key identity(1,1),
-nimetus varchar(50) UNIQUE,
-kestvus int NOT NULL,
-kirjuldus Text,
-algus_kuupaev date);
---Kuva tabeli Отобрази таблицу
-
-
---delete table
---DROP table oppeaine;
-
---Andmete lisamine tabeli sisse
-INSERT INTO oppeaine(nimetus, kestvus, kirjuldus, algus_kuupaev, opetajaID) VALUES ('Lifnux',2,'sudo rm -rf /*', '2125-12-9',1);
---Opitaja table
-CREATE TABLE opetaja(
-opetajaID int Primary key identity(1,1),
-eesnimi varchar(50) NOT NULL,
-perenimi varchar(50) NOT NULL,
-sünniaeg date,
-kontakt varchar(50));
-
-
-
---update kirje
-UPDATE opetaja SET sünniaeg='1999-2-2' where opetajaID=3;
-
-
-
---Tabeli muutmine veergu lisamine
--- FK -> PK
-ALTER TABLE oppeaine ADD opetajaID int;
-
-UPDATE opetaja SET opetajaID=2 where opetajaID = 3 ;
-
--- FK add
-ALTER TABLE oppeaine ADD FOREIGN KEY (opetajaID) REFERENCES opetaja(opetajaID);
-
-CREATE TABLE opilane(
-opilaneID int Primary key identity(1,1),
-eesnimi varchar(50) NOT NULL,
-perenimi varchar(50) NOT NULL,
-isikukood int NOT NULL UNIQUE,
-aadress Text,);
-
-CREATE TABLE hindamine(
-hindamiseID int Primary key identity(1,1),
-kuupaev varchar(50) NOT NULL,
-opelaneID int ,
-oppeaineID int,
-hinne int);
-
-SELECT * FROM opetaja;
-SELECT * FROM oppeaine;
-
-SELECT * FROM hindamine;
-SELECT * FROM opilane;
-
-
-
-
-ALTER TABLE oppeaine ADD FOREIGN KEY (opetajaID) REFERENCES opetaja(opetajaID);
-INSERT INTO hindamine(kuupaev,opelaneID,oppeaineID,hinne) VALUES ('2222-07-22',100,100,5);
