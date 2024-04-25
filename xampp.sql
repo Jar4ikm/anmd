598d76f3c2f6cf7d3e17cab98c4a744a83370cc2
Jar4ik <119585243+Jar4ikm@users.noreply.github.com>
2024-04-25 10:41:26 +0300

    Update xampp.sql

--- xampp.sql
+++ xampp.sql
@@ -42,3 +42,100 @@ BEGIN
          HAVING SUM(hind) >= 10;
       END CASE;
       END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------
+
+
+CREATE TABLE kaup(
+kaupID int Primary key identity(1,1),
+kaup varchar(50) UNIQUE,
+hind int,
+kirjeldus Text);
+
+INsert into kaup (kaup,hind,kirjeldus,kaal) values ('feather',2,'feather from a bird',10)
+
+CREATE PROCEDURE firs
+    @str varchar(4)
+AS
+BEGIN 
+
+    IF (@str = 'sum1')
+    BEGIN
+        SELECT kaup, SUM(hind) as sum1
+        FROM kaup
+        GROUP BY kaup
+        HAVING SUM(hind) < 5;
+    END
+    ELSE IF (@str = 'sum2')
+    BEGIN
+        SELECT kaup, SUM(hind) as sum2
+        FROM kaup
+        GROUP BY kaup
+        HAVING SUM(hind) >= 5 and SUM(hind) < 10;
+    END
+    ELSE IF (@str = 'sum3')
+    BEGIN
+        SELECT kaup, SUM(hind) as sum2
+        FROM kaup
+        GROUP BY kaup
+        HAVING SUM(hind) >= 10;
+    END
+    ELSE
+    BEGIN
+        PRINT 'Invalid input';
+    END;
+END;
+
+exec firs 'sum3'
+select * from kaup
+UPDATE kaup SET kaal=1000 where kaupID=3;
+
+
+CREATE PROCEDURE secon
+    @str varchar(4)
+AS
+BEGIN 
+
+    IF (@str = 'sum1')
+    BEGIN
+        SELECT kaup, SUM(kaal) as sum1
+        FROM kaup
+        GROUP BY kaup
+        HAVING SUM(kaal) < 100;
+    END
+    ELSE IF (@str = 'sum2')
+    BEGIN
+        SELECT kaup, SUM(kaal) as sum2
+        FROM kaup
+        GROUP BY kaup
+        HAVING SUM(kaal) >= 100 and SUM(kaal) < 500;
+    END
+    ELSE IF (@str = 'sum3')
+    BEGIN
+        SELECT kaup, SUM(kaal) as sum2
+        FROM kaup
+        GROUP BY kaup
+        HAVING SUM(kaal) >= 500;
+    END
+    ELSE
+    BEGIN
+        PRINT 'Invalid input';
+    END;
+END;
+
+exec secon 'sum1'
